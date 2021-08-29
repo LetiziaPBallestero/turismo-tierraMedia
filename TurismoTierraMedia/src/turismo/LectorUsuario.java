1a6fdbdebd56ec43a51e0910cd6a3d70a227dfed
Paula <pau.alzamora@gmail.com>
2021-08-29 19:54:17 -0300

    Se agregó LectorUsuario, constructor de clase Usuario y usuarios.txt

--- TurismoTierraMedia/src/turismo/LectorUsuario.java
+++ TurismoTierraMedia/src/turismo/LectorUsuario.java
@@ -1,5 +1,52 @@
 package turismo;
 
-public class LectorUsuario {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class LectorUsuario {
+	
+	public void leerUsuario() {
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda (disponer del metodo readLine()).
+			fr = new FileReader("usuarios.txt");
+			br = new BufferedReader(fr);
+			// Lectura del fichero
+			String linea = br.readLine();
+			while ((linea != null)) {
+				this.crearEImprimirUsuario(linea);
+				linea = br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// En el finally cerramos el fichero, para asegurarnos
+			// que se cierra tanto si todo va bien como si salta
+			// una excepcion.
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	private Usuario crearEImprimirUsuario(String linea) {
+		String lin[] = linea.split(",");
+		System.out.println(new Usuario(TipoDeAtraccion.valueOf(lin[0]), lin[1],
+				Integer.parseInt(lin[2]), Double.parseDouble(lin[3])).toString());
+		return new Usuario(TipoDeAtraccion.valueOf(lin[0]), lin[1],
+				Integer.parseInt(lin[2]), Double.parseDouble(lin[3]));
+	}
+	
+	public static void main(String[] args) {
+		LectorUsuario lectorUsuario = new LectorUsuario();
+		lectorUsuario.leerUsuario();
+	}
 }
--- TurismoTierraMedia/src/turismo/Usuario.java
+++ TurismoTierraMedia/src/turismo/Usuario.java
@@ -1,5 +1,24 @@
 package turismo;
 
 public class Usuario {
+	private TipoDeAtraccion tipoDeAtraccionPreferido;
+	private String nombre;
+	private int presupuesto;
+	private double tiempoDisponible;
+	
+	public Usuario(TipoDeAtraccion tipoDeAtraccionPreferido, String nombre, int presupuesto, double tiempoDisponible) {
+		super();
+		this.tipoDeAtraccionPreferido = tipoDeAtraccionPreferido;
+		this.nombre = nombre;
+		this.presupuesto = presupuesto;
+		this.tiempoDisponible = tiempoDisponible;
+	}
 
+	@Override
+	public String toString() {
+		return "Usuario [Tipo de atracción preferido = " + tipoDeAtraccionPreferido + ", Nombre = " + nombre + ", Presupuesto = "
+				+ presupuesto + " monedas, Tiempo disponible = " + tiempoDisponible + " horas]";
+	}
+	
+	
 }
