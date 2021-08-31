3c7e6135abd02eadcaeea543c8b5609771b06c89
Franco Garcete <francojg2013.Fg@gmail.com>
2021-08-31 16:55:30 -0300

    Primer intento de la clase Atraccion.
    Revisar nuevamente despues de ver la clase Producto.

--- TurismoTierraMedia/src/turismo/Atraccion.java
+++ TurismoTierraMedia/src/turismo/Atraccion.java
@@ -1,5 +1,31 @@
 package turismo;
 
-public class Atraccion extends Producto {
+public class Atraccion extends Producto
+{
+	private int costoAtraccion;
+	private double tiempo;
+	private int cupoMaximo;
+	private int cuposOcupados;
+	private TipoDeAtraccion tipoDeAtraccion;
+
+	public int getCosto()
+	{
+		return costoAtraccion;
+	}
+
+	public double getTiempo()
+	{
+		return tiempo;
+	}
+
+	public TipoDeAtraccion getTipoDeAtraccion()
+	{
+		return tipoDeAtraccion;
+	}
+
+	public boolean hayCupo(int cuposOcupados)
+	{
+		return cuposOcupados < cupoMaximo;
+	}
 
 }
