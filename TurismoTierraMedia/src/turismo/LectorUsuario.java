9550a5abcabd3188dfb05ecab43fbe53a99cd27b
Paula <pau.alzamora@gmail.com>
2021-08-31 21:29:36 -0300

    Cambio de extension archivo usuarios, se agrega la ArrayList<Usuario>

--- TurismoTierraMedia/src/turismo/LectorUsuario.java
+++ TurismoTierraMedia/src/turismo/LectorUsuario.java
@@ -3,17 +3,21 @@ package turismo;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import java.util.List;
 
 public class LectorUsuario {
 	
-	public void leerUsuario() {
+	public void leerUsuario() throws UsuarioException {
+		List <Usuario> usuarios = new ArrayList<Usuario>();
 		FileReader fr = null;
 		BufferedReader br = null;
 
 		try {
 			// Apertura del fichero y creacion de BufferedReader para poder
 			// hacer una lectura comoda (disponer del metodo readLine()).
-			fr = new FileReader("src/archivosDeEntrada/usuarios.txt");
+			fr = new FileReader("src/archivosDeEntrada/usuarios.csv");
 			br = new BufferedReader(fr);
 			// Lectura del fichero
 			String linea = br.readLine();
@@ -37,15 +41,18 @@ public class LectorUsuario {
 		}
 	}
 	
-	private Usuario crearEImprimirUsuario(String linea) {
+	private Usuario crearEImprimirUsuario(String linea) throws UsuarioException {
 		String lin[] = linea.split(",");
-		System.out.println(new Usuario(TipoDeAtraccion.valueOf(lin[0]), lin[1],
+		if(lin.length!=4) {
+			throw new UsuarioException("Cantidad de parámetros errónea");
+		}
+		System.out.println(new Usuario(TipoDeAtraccion.valueOf(lin[0].toUpperCase()), lin[1],
 				Integer.parseInt(lin[2]), Double.parseDouble(lin[3])).toString());
 		return new Usuario(TipoDeAtraccion.valueOf(lin[0]), lin[1],
 				Integer.parseInt(lin[2]), Double.parseDouble(lin[3]));
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UsuarioException {
 		LectorUsuario lectorUsuario = new LectorUsuario();
 		lectorUsuario.leerUsuario();
 	}
