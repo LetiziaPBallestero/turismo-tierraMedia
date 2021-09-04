acd35286d19445ce65ada28e7274cbd8daa24284
Paula <pau.alzamora@gmail.com>
2021-09-04 14:04:04 -0300

    Constructor PromocionAbsoluta

--- TurismoTierraMedia/src/turismo/PromocionAbsoluta.java
+++ TurismoTierraMedia/src/turismo/PromocionAbsoluta.java
@@ -1,12 +1,14 @@
 package turismo;
 
+import java.util.List;
+
 public class PromocionAbsoluta extends Promocion {
 	private String tipo;
 	private int costo;
+	private List<Atraccion> atracciones;
 
-	public PromocionAbsoluta(int costo, double tiempo, TipoDeAtraccion tipoAtraccion) {
+	public PromocionAbsoluta(TipoDeAtraccion tipoAtraccion, int costo, double tiempo) {
 		super(costo, tiempo, tipoAtraccion);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- TurismoTierraMedia/src/turismo/TierraMedia.java
+++ TurismoTierraMedia/src/turismo/TierraMedia.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class TierraMedia {
 	private static List<Usuario> usuarios;
-	private static List<Producto> productos = new ArrayList<Producto>();
+	private static List<Producto> productos = new LinkedList<Producto>();
 	
 	public static void main(String[] args) throws UsuarioException, AtraccionException {
 		List<Atraccion> atracciones = new LinkedList<Atraccion>();
