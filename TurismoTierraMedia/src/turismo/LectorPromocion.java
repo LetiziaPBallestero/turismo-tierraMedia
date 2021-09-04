2fab16fae2b2ae433c4a29638d8d563d1c46eb7b
ballestero letizia <letiziapaolaballestero@gmail.com>
2021-09-04 11:06:00 -0300

    Lector de promociones agregado

--- TurismoTierraMedia/src/turismo/LectorPromocion.java
+++ TurismoTierraMedia/src/turismo/LectorPromocion.java
@@ -1,4 +1,39 @@
 package turismo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LectorPromocion {
+	
+	public void leerPromocion() {
+		FileReader fr = null; 
+		BufferedReader br = null; 
+		
+		try {
+			fr = new FileReader ("promociones.txt"); 
+			br = new BufferedReader (fr); 
+			
+			String linea = br.readLine (); 
+			while (( linea != null)) {
+				this.crearEImprimirPromocion (linea);
+				linea = br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			}catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	private Promocion crearEImprimirPromocion (string linea) {
+	// antes de crear este método tengo que consultar cómo va a ser el archivo de promocion 
+	
+	
 }
